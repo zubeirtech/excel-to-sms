fd08b1a3f8a8d51aedd1be97af1ebada7bfb4729
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-13 04:45:51 +0200

    wip

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const express = require("express");
 const { extractExcel } = require("./services/excelExtractor");
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 const { sendSms } = require("./services/smsMessenger");
-// const cors = require(cors());
+const cors = require("cors");
 
-// const app = express();
+const app = express();
 
-// const port  = 3000;
+const port  = 3000;
 
-// app.use(cors())
+app.use(cors())
 
-extractExcel("Book.xlsx").then(res => {
-    console.log(res.length);
-})
+// extractExcel("Book.xlsx").then(res => {
+//     console.log(res.length);
+// })
 
 //sendSms();
 
+app.get('/token', async (req, res) => {
+    try {
+        const { username, password, grant_type } = req.body;
+        
+        const raw = fs.readFileSync('./db.json');
+        const db = JSON.parse(raw);
+        if (grant_type === 'password') {
+            if(username === db.username && password === db.password) {
+                const token = await jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
+                res.status(200).send(`{ "access_token": "${token}"}`);
+            }
+        } else {
+            res.status(400).send('{"error": "invalid_grant"}');
+            next(errorMessage);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('{ "error": "unsupported_grant_type" }');
+        next(errorMessage);
+    }
+})
 
+
+const { PORT } = process.env;
+app.listen(PORT || 3000, console.log(`Server started on port ${PORT}`));
