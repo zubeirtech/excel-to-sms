ea238569f5c263cff0fc539eaf519d9930ef624e
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-13 15:19:59 +0200

    wip

--- client/app/controllers/settings.js
+++ client/app/controllers/settings.js
@@ -1,12 +1,34 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import ENV from "client/config/environment";
 
 export default Controller.extend({
     session: service(),
+    ajax: service(),
+    toastr: service('toast'),
 
     actions: {
-        logout() {
-            this.session.invalidate();
+        save() {
+            try {
+                if(this.username || this.password) {
+                    const res = await this.ajax.request(`${ENV.host}/change-credentials`, {
+                        method: 'POST',
+                        data: {
+                            username: this.username,
+                            password: this.password
+                        }
+                    })
+
+                    if(res.body.success) {
+                        this.toastr.success("Saved changes", "Done");
+                    }
+                } else {
+                    this.toastr.error("Please fill either password or username", "Error");
+                }
+            } catch (error) {
+                this.toastr.error("Something went wrong", "Error");
+                throw error;
+            }
         }
     }
 });
