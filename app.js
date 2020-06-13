8b7448cc8b7f481a746d430cd231fc4a23c7b8d2
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-13 02:46:58 +0200

    test with large excel

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const cors = require(cors());
+const { extractExcel } = require("./services/excelExtractor");
+// const cors = require(cors());
 
-const app = express();
+// const app = express();
 
-const port  = 3000;
+// const port  = 3000;
 
-app.use(cors())
+// app.use(cors())
+
+extractExcel("Book.xlsx").then((res) => {
+    console.log(res);
+});
 
 
--- services/excelExtractor.js
+++ services/excelExtractor.js
@@ -1,1 +1,16 @@
+const xlsxFile = require('read-excel-file/node');
 
+
+module.exports = {
+    
+    async extractExcel(fileName) {
+        const numbers = [];
+        await xlsxFile(`./files/${fileName}`).then((rows) => {
+            rows.forEach(col => {                
+                numbers.push(col[0].toString());
+            });
+        });
+
+        return numbers;
+    }
+}
