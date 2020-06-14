aa7ea3aeb1996ffbcf18682b039f64b9154ff3d2
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-14 05:24:37 +0200

    push cors to the top

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const cors = require("cors");
 const app = express();
 
 const port = 3000;
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors());
 app.use(fileUpload());
 
 
